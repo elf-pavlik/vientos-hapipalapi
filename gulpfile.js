7d1629eaad07ecbdf9084610e220b610bcd64234
elf Pavlik <elf-pavlik@hackers4peace.net>
2017-01-23 16:32:49 -0600

    started preparing for use with vientos-dev-env

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,72 @@
-var gulp = require('gulp'),
-  server = require('gulp-develop-server')
+var gulp = require('gulp')
+var server = require('gulp-develop-server')
 
 gulp.task('default', ['server:start', 'server:restart'])
 
-// run server 
+// run server
 gulp.task('server:start', function () {
   server.listen({ path: './server.js' })
 })
 
-// restart server if app.js changed 
+// restart server if app.js changed
 gulp.task('server:restart', function () {
   gulp.watch([ './server.js' ], server.restart)
 })
+
+const MONGO_URL = 'mongodb://localhost:27017'
+gulp.task('seed:user', () => {
+  const UserManager = require('./src/managers/user')
+  const Bcrypt = require('bcrypt-nodejs')
+  const MongoClient = require('mongodb').MongoClient
+  let password = 'lol'
+  let user = {
+    email: 'me@example.org',
+    password: Bcrypt.hashSync(password),
+    username: 'me'
+  }
+  MongoClient.connect(MONGO_URL, (err, db) => {
+    if (err) throw err
+    UserManager.insert(db, user, (doc) => {
+      console.log('username:', user.username)
+      console.log('password:', password)
+      return db.close()
+    })
+  })
+})
+
+gulp.task('seed:projects', () => {
+  const ProjectManager = require('./src/managers/project')
+  const MongoClient = require('mongodb').MongoClient
+  const data = require('vientos-dev-data')
+  MongoClient.connect(MONGO_URL, (err, db) => {
+    if (err) throw err
+    Promise.all(data.projects.map(project => {
+      return ProjectManager.insert(db, project, (doc) => { return Promise.resolve(doc) })
+    })).then((all) => {
+      console.log('imported', all.length, 'projects')
+      db.close()
+    })
+  })
+})
+
+gulp.task('db:drop', () => {
+  const MongoClient = require('mongodb').MongoClient
+  MongoClient.connect(MONGO_URL, (err, db) => {
+    if (err) throw err
+    db.dropDatabase().then(() => db.close())
+  })
+})
+
+gulp.task('db:stats', () => {
+  const MongoClient = require('mongodb').MongoClient
+  MongoClient.connect(MONGO_URL, (err, db) => {
+    if (err) throw err
+    db.collection('users').count()
+      .then(count => console.log('users:', count))
+      .then(() => db.collection('projects').count())
+      .then(count => console.log('projects:', count))
+      .then(() => db.close())
+  })
+})
+
+gulp.task('seed', ['db:drop', 'seed:user', 'seed:projects', 'db:stats'])
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 'use strict'
 const Hapi = require('hapi')
 var routes = require('./src/routes')
-var credentials = require('./credentials.json')
 
+const DEFAULT_PORT = 3200
+const DEFAULT_COOKIE_PASS = 'set me in COOKIE_PASS ENV variable'
 const server = new Hapi.Server()
 
 // Definición de puerto donde correrá el servidor
 server.connection({
-  port: credentials.server_port ,
+  port: process.env.HAPI_PORT || DEFAULT_PORT,
   routes: { cors: { credentials: true}}
 })
 
 // Datos de Configuración de Base de Datos
 var dbOpts = {
-  'url': credentials.mongo_url, // TODO debe de venir de ENV
   'decorate': true,
   'settings': {
     'db': {
@@ -21,6 +21,8 @@ var dbOpts = {
     }
   }
 }
+
+if (process.env.MONGO_URL) dbOpts.url = process.env.MONGO_URL
 
 // Registro de plugin hapi-mongodb
 server.register(
@@ -39,7 +41,7 @@ server.register(
 // Registro de plugin hapi-auth-cokie y definición de estrategia de autenticación
 server.register(require('hapi-auth-cookie'), function (err) {
   server.auth.strategy('standard', 'cookie', true, { // if true, all routes have authentication by default
-    password: credentials.cookie_pass, // TODO debe de venir de ENV
+    password: process.env.COOKIE_PASS || DEFAULT_COOKIE_PASS,
     cookie: 'sidVientos',
     redirectTo: '/login',
     isSecure: false,
