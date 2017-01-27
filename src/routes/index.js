90485f70719f3c0acc8b55f442064ff3abb409b5
elf Pavlik <elf-pavlik@hackers4peace.net>
2017-01-27 12:15:45 -0600

    deleted private/public pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,2 @@
-//API
 exports.projectApi = require('./api/projectApi.js')
 exports.userApi = require('./api/userApi.js')
-//NOT API
-exports.publicPages = require('./publicPages.js')
-exports.privatePages = require('./privatePages.js')
